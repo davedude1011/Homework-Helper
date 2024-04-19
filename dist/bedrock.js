31468a1dc45540bd270aa7fa1365f51da4d8f41d
davedude1011 <151575930+davedude1011@users.noreply.github.com>
2024-04-19 16:06:35 +0100

    Add files via upload

--- dist/bedrock.js
+++ dist/bedrock.js
@@ -14,18 +14,116 @@ GetWeeklyProgress();
 function SetWeeklyProgress() {
     let Outer = document.querySelector(".user-controls-group");
     let Points = GetWeeklyProgress();
-    if (Outer && Points) {
-        Outer.innerHTML = `<span>Bedrock Points: (${Points}/20)</span>`;
+    if (Outer) {
+        Outer.innerHTML = `<span>Points: (<b>${Points}</b>/20)</span>`;
+    }
+    DictionaryButton();
+}
+setInterval(SetWeeklyProgress, 50);
+function SetDictionary(Word) {
+    var _a, _b;
+    let OuterElement = document.querySelector(".quiz-questions") || document.querySelector(".panel--learning");
+    if (OuterElement) {
+        OuterElement.style.display = "flex";
+        OuterElement.style.flexDirection = "row-reverse";
+        let DictionaryElement = document.querySelector(".dictionary");
+        if (DictionaryElement) {
+            DictionaryElement.src = `https://www.merriam-webster.com/dictionary/${Word}`;
+        }
+        else {
+            let DictionaryElement = document.createElement("iframe");
+            DictionaryElement.src = `https://www.merriam-webster.com/dictionary/${Word}`;
+            DictionaryElement.classList.add("dictionary");
+            DictionaryElement.style.width = "30%";
+            OuterElement.appendChild(DictionaryElement);
+        }
+    }
+    let LaptopContainer = (_b = (_a = document.querySelector(".panel--pad.background_image.extra_padded")) === null || _a === void 0 ? void 0 : _a.parentElement) === null || _b === void 0 ? void 0 : _b.parentElement;
+    if (LaptopContainer) {
+        LaptopContainer.style.width = "70%";
     }
 }
-SetWeeklyProgress();
-window.addEventListener("click", function () {
+function RemoveDictionary() {
+    var _a, _b, _c, _d, _e;
+    (_a = document.querySelector(".dictionary")) === null || _a === void 0 ? void 0 : _a.remove();
+    (_c = (_b = document.querySelector(".no-select")) === null || _b === void 0 ? void 0 : _b.parentNode) === null || _c === void 0 ? void 0 : _c.removeChild(document.querySelector(".no-select"));
+    let LaptopContainer = (_e = (_d = document.querySelector(".panel--pad.background_image.extra_padded")) === null || _d === void 0 ? void 0 : _d.parentElement) === null || _e === void 0 ? void 0 : _e.parentElement;
+    if (LaptopContainer) {
+        LaptopContainer.style.width = "100%";
+    }
+}
+function DictionaryButton() {
+    var _a;
+    let DictionaryButton = document.querySelector(".dictionary-button");
+    if (DictionaryButton) { }
+    else {
+        let VocabOuter = document.querySelector(".learning-controls");
+        if (VocabOuter) {
+            let NewButton = document.createElement("div");
+            NewButton.innerHTML = `<span _ngcontent-c5="" class="lcb-label">Toggle Dictionary</span>`;
+            NewButton.classList.add("lc-button");
+            NewButton.classList.add("lc-button--iconMobile");
+            NewButton.classList.add("dictionary-button");
+            NewButton.addEventListener("click", function () {
+                isCalculator = !isCalculator;
+                if (!isCalculator) {
+                    RemoveDictionary();
+                }
+                else {
+                    SetDictionary("");
+                    var selectionStyle = document.createElement('style');
+                    selectionStyle.classList.add("no-select");
+                    selectionStyle.textContent = `
+                        ::selection {
+                            background: transparent;
+                        }
+                    `;
+                    document.head.appendChild(selectionStyle);
+                }
+            });
+            let ExitButton = (_a = VocabOuter.querySelector(".lc-button .fa-times-circle")) === null || _a === void 0 ? void 0 : _a.parentElement;
+            if (ExitButton) {
+                VocabOuter.insertBefore(NewButton, ExitButton);
+            }
+        }
+    }
+}
+window.addEventListener("click", function (event) {
     let SelectableTextStyle = ":root { --user-select-accessibility-setting: default !important; }";
     let SelectableTextStyleElement = document.createElement("style");
     SelectableTextStyleElement.innerHTML = SelectableTextStyle;
     document.head.appendChild(SelectableTextStyleElement);
-    setTimeout(function () {
-        SetWeeklyProgress();
-    }, 200);
+    try {
+        let ClickedElement = event.target;
+        if (ClickedElement && ClickedElement.classList.contains("ready")) {
+            if (isCalculator) {
+                isCalculator = false;
+                RemoveDictionary();
+            }
+        }
+    }
+    catch (_a) { }
 });
+window.addEventListener("contextmenu", function (event) {
+    if (isCalculator) {
+        event.preventDefault();
+        var clickedElement = event.target;
+        if (clickedElement.nodeType === Node.TEXT_NODE) {
+            clickedElement = clickedElement.parentNode;
+        }
+        var selection = window.getSelection();
+        var range = selection.getRangeAt(0);
+        var node = range.startContainer;
+        while (range.toString().indexOf(' ') != 0 && range.startOffset > 0) {
+            range.setStart(node, range.startOffset - 1);
+        }
+        range.setStart(node, range.startOffset + 1);
+        do {
+            range.setEnd(node, range.endOffset + 1);
+        } while (range.toString().indexOf(' ') == -1 && range.toString().trim() != '');
+        var word = range.toString().trim().replace(/[^a-zA-Z]/g, '');
+        SetDictionary(word);
+    }
+});
+var isCalculator = false;
 //# sourceMappingURL=bedrock.js.map
